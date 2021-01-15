c537f9033059d3248da655d4280e1ba2afffbab5
spss20 <sppro.20@gmail.com>
2021-01-15 17:33:33 +0530

    Strapi Version update

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 80),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '060d06bc1394a99ba196f96f570a10f9'),
