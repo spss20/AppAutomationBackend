00a4afdfc1ca1f09424a9dc72a2cb9280fbed55d
spss20 <sppro.20@gmail.com>
2021-02-25 11:17:34 +0530

    Order api changed

--- api/orders/controllers/orders.js
+++ api/orders/controllers/orders.js
@@ -8,7 +8,6 @@ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 module.exports = {
 
     async create(ctx) {
-
         //getting user id
         const { id } = await strapi.plugins[
             'users-permissions'
@@ -25,5 +24,45 @@ module.exports = {
             entity = await strapi.services.orders.create(ctx.request.body);
         }
         return sanitizeEntity(entity, { model: strapi.models.orders });
+    },
+    async find(ctx) {
+
+        //getting user id
+        const { id } = await strapi.plugins[
+            'users-permissions'
+        ].services.jwt.getToken(ctx);
+        console.log("User id is ", id)
+
+        let entities;
+        ctx.query.user = id;
+        if (ctx.query._q) {
+            console.log("Query ", ctx.query)
+            entities = await strapi.services.orders.search(ctx.query);
+        } else {
+            console.log("Raw ", ctx.query)
+            entities = await strapi.services.orders.find(ctx.query);
+        }
+
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.orders }));
+    },
+
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        //getting user id
+        const userData = await strapi.plugins[
+            'users-permissions'
+        ].services.jwt.getToken(ctx);
+        console.log("User id is ", userData.id)
+
+        if (userData.id == id) {
+            const entity = await strapi.services.orders.findOne({ id });
+            return sanitizeEntity(entity, { model: strapi.models.orders });
+        } else {
+            ctx.status = 403
+            return {
+                "status": 403,
+                "error": "Access Forbidden"
+            };
+        }
     },
 };
