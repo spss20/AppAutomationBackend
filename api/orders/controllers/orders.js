27cbb78ca72c04d3f7f01fee88a542c87e9f8ba6
spss20 <sppro.20@gmail.com>
2021-02-25 10:14:17 +0530

    Order api changed, user relation added

--- api/orders/controllers/orders.js
+++ api/orders/controllers/orders.js
@@ -1,8 +1,29 @@
 'use strict';
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    async create(ctx) {
+
+        //getting user id
+        const { id } = await strapi.plugins[
+            'users-permissions'
+        ].services.jwt.getToken(ctx);
+
+        //Create database entry
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            data.user = [id];
+            entity = await strapi.services.orders.create(data, { files });
+        } else {
+            ctx.request.body.user = [id];
+            entity = await strapi.services.orders.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.orders });
+    },
+};
